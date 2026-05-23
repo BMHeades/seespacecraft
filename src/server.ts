9b4e86fd62e7c93da77abc993bf9c897432816fb
bmheades <bmheades@gmail.com>
2026-05-22 23:37:27 -0700

    added cors allowing all sites

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { Request, Response, NextFunction } from "express";
 import { horizons } from "./horizons.js";
 import 'dotenv/config'
@@ -44,6 +45,7 @@ scheduler()
 
 // middlewares
 app.use(express.json())
+app.use(cors())
 app.use(logResponses)
 
 function logResponses(req: Request, res: Response, next: NextFunction): void {
